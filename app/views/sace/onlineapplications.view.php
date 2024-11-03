42c6c5993060cd1887ca7414bc17455b67ce01fb
opcode <suarezdarlin41@gmail.com>
2024-11-03 17:43:25 -0600

    Layout view tools for teachers

--- app/views/sace/onlineapplications.view.php
+++ app/views/sace/onlineapplications.view.php
@@ -2,19 +2,24 @@
     <h4 class="fw-bold fs-1 col-12 text-center mb-4 animated bounceInDown title-online-app">SERVICIOS EN LÍNEA DE LA DIRECCIÓN DEPARTAMENTAL DE EDUCACIÓN COPÁN
     </h4>
     <div class="col-12 col-md-4">
-        <a href="https://forms.gle/opeXZnvfBaiGY6oF6" class="card text-center shadow text-decoration-none p-3 animated bounceInLeft card-online-app" style="height: 200px;" target="_blank">
-            <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                <h5 class="card-title">Solicitud en Línea de Certificación de Trabajo (SECRETARÍA)</h5>
+        <a href="https://forms.gle/opeXZnvfBaiGY6oF6" class="glass-card mt-2 text-light text-decoration-none p-0 animated bounceInLeft">
+            <div class="z-3 px-2 text-center text-dark">
+                <h5>Solicitud en Línea de Certificación de Trabajo (SECRETARÍA)</h5>
                 <i class="fa-regular fa-file-lines fs-1 text-blue"></i>
             </div>
+            <div class="glass-card-bg z-2"></div>
+            <div class="glass-card-blob z-1"></div>
         </a>
     </div>
     <div class="col-12 col-md-4">
-        <a href="<?php echo $env->APP_URL ?>uti" class="card text-center shadow text-decoration-none p-3 animated bounceInRight card-online-app" style="height: 200px;">
-            <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                <h5 class="card-title border-bottom-1 border-primary">Solicitud en Línea de Operaciones en SACE (UTI)</h5>
+        <a href="<?php echo $env->APP_URL ?>uti" class="glass-card mt-2 text-light text-decoration-none p-0 animated bounceInRight">
+            <div class="z-3 px-2 text-center text-dark">
+                <h5>Solicitud en Línea de Operaciones en SACE (UTI)</h5>
                 <i class="fa-solid fa-laptop-file fs-1 text-blue"></i>
+
             </div>
+            <div class="glass-card-bg z-2"></div>
+            <div class="glass-card-blob z-1"></div>
         </a>
     </div>
 </div>
--- app/views/tools/teachers.view.php
+++ app/views/tools/teachers.view.php
@@ -1,10 +1,231 @@
 <div class="row px-3 px-md-4 px-lg-5 my-4">
-    <h4 class="fw-bold fs-1 col-12 text-center title-online-app">
+    <h1 class="fw-bold fs-1 col-12 text-center title-online-app text-uppercase">
         Herramientas para docentes
-    </h4>
+    </h1>
 
-    <h5 class="text-blue fw-bold col-12 mt-4">Herramientas desarolladas por la dirección departamental Copán</h5>
-    <div class="col-12">
-
+    <h5 class="text-blue fw-bold col-12 mt-5 px-0">Herramientas desarolladas por la dirección departamental Copán</h5>
+    <div class="row p-5">
+        <div class="col-12 col-md-4">
+            <a href="" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Solicitudes en Línea</h5>
+                    <p class="z-3">Solicitar y administrar la información de estudiantes, docentes y personal administrativo.</p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
     </div>
+
+    <h5 class="text-blue fw-bold col-12 mt-5 px-0">Herramientas desarolladas por la Secretaría de Educación</h5>
+    <div class="row p-5">
+        <div class="col-12 col-md-4">
+            <a href="<?php echo $APP_URL ?>sace/onlineapplications" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Solicitudes en Línea</h5>
+                    <p class="z-3">Solicitar y administrar la información de estudiantes, docentes y personal administrativo.</p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://sace.se.gob.hn/" target="_blank" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Ingresar a SACE</h5>
+                    <p class="z-3">Ingresar a la plataforma del sistema de administración de centros educativos.</p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://sace.se.gob.hn/media/comunicados/oficio-003-ssatp-2023_lineamientos-de-apertura-2023.pdf" target="_blank" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Normas de evaluación</h5>
+                    <p class="z-3">Normas generales para evaluación de los aprendizajes en los niveles de educación prebásica, básica y media en sus diferentes modalidades de entrega
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Instituto de Acceso a la Información Pública</h5>
+                    <p class="z-3">Ingresar al sitio web del IAIP</p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Archivística</h5>
+                    <p class="z-3">Documentos del Seminario de Archivística 17 y 18 de Mayo de 2024</p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Plataformas Virtuales</h5>
+                    <p class="z-3">Plataformas en línea de apoyo de la Secretaría de Educación.</p>
+
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Ficha de Centro Educativo</h5>
+                    <p class="z-3">Busque la Ficha de su Centro Educativo aquí.</p>
+
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Concurso Docente</h5>
+                    <p class="z-3">Resultados Preliminares del Concurso Docente 2023.
+                    </p>
+
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://drive.google.com/drive/folders/1fOyr60BDpH0QuOvocf8v6VTT0ITBNiDv" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Guía de Orientaciones Metodológicas 2023</h5>
+                    <p class="z-3">Guía de Orientaciones Metodológicas para Prebásica, Básica y Media 2023.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Portal de Transparencia</h5>
+                    <p class="z-3">Portal de Transparencia de la Dirección Departamental de Educación de Copán.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://educatrachos.learningpassport.unicef.org/" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Pasaporte de Aprendizaje</h5>
+                    <p class="z-3">Plataforma MOODLE para el aprendizaje Online con apoyo de Microsoft, Unicef, Usaid y Learning Passport.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://cevirtual.se.gob.hn/cevirtual/" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Centro Educativo Virtual</h5>
+                    <p class="z-3">Plataforma MOODLE para el aprendizaje Online.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://www.se.gob.hn/se-priorizacion-curricular/" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Priorizacion Curricular de Todos los Grados</h5>
+                    <p class="z-3">Priorización de temas de todas las Asignatura de Prebásica, Básica 1ero y 2do Ciclo, 3er ciclo y Media.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://siap.se.gob.hn/upload/index.php" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Portal único de Transparencia de Datos</h5>
+                    <p class="z-3">Plataforma tecnológica gratuita, creada por el IAIP para facilitar a las personas el ejercicio del derecho de acceso a la información
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://educatrachos.odilotk.es/" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Biblioteca Digital</h5>
+                    <p class="z-3">Biblioteca Virtual Educatrachos de la secretaria de Educación de Honduras.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://se.gob.hn/perfil-usinieh/" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">UNISIEH</h5>
+                    <p class="z-3">Portal de Recursos Tecnológicos
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="http://chorti.sefin.gob.hn/forms/frmservlet?config=produccion" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Secretaría de Bienes Nacionales del Departamento de Copán</h5>
+                    <p class="z-3">Verificación de Bienes Nacionales del Departamento de Copán.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+    </div>
+    <h5 class="text-blue fw-bold col-12 mt-5 px-0">Herramientas desarolladas Telebásica para uso Docente.</h5>
+    <div class="row p-5">
+        <div class="col-12 col-md-4">
+            <a href="<?php echo $APP_URL ?>sace/onlineapplications" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Libros y Guías Docentes Telebásica</h5>
+                    <p class="z-3">Libros Completos en formato PDF para 3er Ciclo (7mo, 8vo y 9no).</p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="<?php echo $APP_URL ?>sace/onlineapplications" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Lecciones Educativas Telebásica</h5>
+                    <p class="z-3">Vídeos de ayuda a la compresión de los Libros de trabajo del Docente y Estudiantes de TELEBÁSICA. 3er Ciclo</p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+    </div>
+
 </div>
