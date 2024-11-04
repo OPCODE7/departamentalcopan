424d68fa275bf5393f70732df0bfe67eb0d666d1
opcode <suarezdarlin41@gmail.com>
2024-11-03 18:23:03 -0600

    Layout students tools

---
+++ app/views/tools/students.view.php
@@ -0,0 +1,55 @@
+<div class="row px-3 px-md-4 px-lg-5 my-4">
+    <h1 class="fw-bold fs-1 col-12 text-center title-online-app text-uppercase">
+        Herramientas para estudiantes
+    </h1>
+
+    <div class="row p-5">
+        <div class="col-12 col-md-4">
+            <a href="https://educatrachos.learningpassport.unicef.org/" target="_blank" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Pasaporte de Aprendizaje</h5>
+                    <p class="z-3">Plataforma MOODLE para el aprendizaje Online con apoyo de Microsoft, Unicef, Usaid y Learning Passport.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://cevirtual.se.gob.hn/cevirtual/" target="_blank" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Centro Educativo Virtual</h5>
+                    <p class="z-3">Plataforma MOODLE para el aprendizaje Online.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+
+        <div class="col-12 col-md-4">
+            <a href="https://educatrachos.odilotk.es/" target="_blank" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Libros</h5>
+                    <p class="z-3">Libros de ayuda escolar.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+        <div class="col-12 col-md-4">
+            <a href="https://fundacionceibal.edu.uy/proyectos/investigacion-de-aprendizaje-a-distancia-y-combinado-para-comunidades-rurales/" target="_blank" class="glass-card mt-3 text-light text-decoration-none p-0">
+                <div class="z-3 px-3 text-center text-dark">
+                    <h5 class="fw-bold z-3">Ceibal</h5>
+                    <p class="z-3">Educación Digital Rural e Inclusiva.
+                    </p>
+                </div>
+                <div class="glass-card-bg z-2"></div>
+                <div class="glass-card-blob z-1"></div>
+            </a>
+        </div>
+
+    </div>
+
+</div>
